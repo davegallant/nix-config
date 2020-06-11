6f8e826b826764e26f68db8230178e84316537b9
Dave Gallant <davegallant@gmail.com>
2020-06-11 14:03:14 -0400

    Update awscli v2 nix overlay

--- .config/nixpkgs/overlays/awscli2.nix
+++ .config/nixpkgs/overlays/awscli2.nix
@@ -3,12 +3,12 @@ rec {
   python3 = with super; super.python3.override {
     packageOverrides = self: super: {
       botocore = super.botocore.overridePythonAttrs (oldAttrs: rec {
-        version = "2.0.0dev19";
+        version = "2.0.0dev25";
         src = fetchFromGitHub {
           owner = "boto";
           repo = "botocore";
-          rev = "ce7b76d296b345d7647f50557606947ad857a786";
-          sha256 = "0g9pvcs94v2znjkzzjnz30021q9bm97fxmlhxvql6im3c6adrqb3";
+          rev = "bf9a885fa0bc0bba0c3c806eeeb60d9ad5f3e069";
+          sha256 = "1llshaxpnz9a7mw4kkz9msdgkzz3in5ws3rvd7l077ghj9jkfz9a";
         };
       });
       prompt_toolkit = super.prompt_toolkit.overridePythonAttrs (oldAttrs: rec {
@@ -25,13 +25,13 @@ rec {
 
   awscli2 = with self; pythonPackages.buildPythonApplication rec {
     pname = "awscli";
-    version = "2.0.15"; # N.B: if you change this, change botocore to a matching version too
+    version = "2.0.21"; # N.B: if you change this, change botocore to a matching version too
 
     src = fetchFromGitHub {
       owner = "aws";
       repo = "aws-cli";
       rev = version;
-      sha256 = "030pw7aa1y13awrpwlsq3ib633fh1z4rzkz8b31x7jg7s1hxrsvr";
+      sha256 = "1lxkdjsl3w9c621byy3gggadhfrw8xcw37x3xci9qszxqc10b467";
     };
 
     postPatch = ''
