8bdcde106bada32b5d8a3898a037de63c8c697c5
Dave Gallant <davegallant@gmail.com>
2020-07-16 19:18:51 -0400

    Remove backlight from polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -10,5 +10,3 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
   MONITOR=$m polybar --reload poly &
 done
-
-echo "Polybar launched..."
