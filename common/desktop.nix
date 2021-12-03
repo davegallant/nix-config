cd832126437ee866b5d57c45e17d3ee25a3398b7
Dave Gallant <davegallant@gmail.com>
2021-12-02 22:31:54 -0500

    Switch to x

--- common/desktop.nix
+++ common/desktop.nix
@@ -4,7 +4,17 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm = {
+    enable = true;
+    wayland = false;
+    nvidiaWayland = false;
+  };
   services.xserver.desktopManager.gnome.enable = true;
+
+  services.xserver.displayManager = {
+    autoLogin.enable = true;
+    autoLogin.user = "dave";
+    job.preStart = "sleep 5";
+  };
 
 }
