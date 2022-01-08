0811bbf07edafd4f3a44461b8167e92fb81b3404
Dave Gallant <davegallant@gmail.com>
2022-01-08 01:56:45 -0500

    Add nvim-scrollbar

--- home/nvim/lua/init.lua
+++ home/nvim/lua/init.lua
@@ -157,6 +157,43 @@ require'nvim-tree'.setup {}
 -- gitsigns
 require('gitsigns').setup()
 
+-- nvim-scrollbar
+require("scrollbar").setup({
+    handle = {
+        text = " ",
+        color = "white",
+    },
+    marks = {
+        Search = { text = { "-", "=" }, priority = 0, color = "orange" },
+        Error = { text = { "-", "=" }, priority = 1, color = "red" },
+        Warn = { text = { "-", "=" }, priority = 2, color = "yellow" },
+        Info = { text = { "-", "=" }, priority = 3, color = "blue" },
+        Hint = { text = { "-", "=" }, priority = 4, color = "green" },
+        Misc = { text = { "-", "=" }, priority = 5, color = "purple" },
+    },
+    excluded_filetypes = {
+        "",
+        "prompt",
+        "TelescopePrompt",
+    },
+    autocmd = {
+        render = {
+            "BufWinEnter",
+            "TabEnter",
+            "TermEnter",
+            "WinEnter",
+            "CmdwinLeave",
+            "TextChanged",
+            "VimResized",
+            "WinScrolled",
+        },
+    },
+    handlers = {
+        diagnostic = true,
+        search = true,
+    },
+})
+
 -- treesitter
 require("nvim-treesitter.configs").setup({
 	ensure_installed = "all",
