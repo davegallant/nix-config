06cfef8f7e5e621b98b9a281b04ee0ae63292394
Dave Gallant <davegallant@gmail.com>
2023-07-09 12:16:31 -0400

    Cleanup flake.nix

--- flake.nix
+++ flake.nix
@@ -2,15 +2,15 @@
   description = "nixos and macos configurations";
 
   inputs = {
-    home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
-    };
-
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs-latest.url = "github:NixOS/nixpkgs/master";
 
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-ld.url = "github:Mic92/nix-ld";
+
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
 
     darwin = {
       url = "github:lnl7/nix-darwin";
