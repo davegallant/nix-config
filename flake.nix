b21ecbdb3b10aef8989424594474a0ff3b2a9fae
Dave Gallant <dave.gallant@gmail.com>
2024-03-10 13:01:45 -0400

    Remove defaultModules

--- flake.nix
+++ flake.nix
@@ -32,69 +32,65 @@
     } @ inputs: {
       nixosConfigurations =
         let
-          modulesDir = ./modules;
           unstable = import nixpkgs-unstable {
             system = "x86_64-linux";
             config.allowUnfree = true;
             config.permittedInsecurePackages = [ ];
           };
-          defaultModules = [
-            home-manager.nixosModules.home-manager
-            ./fonts.nix
-            ./packages.nix
-            ./upgrade-diff.nix
-
-            ({ config
-             , lib
-             , ...
-             }: {
-              config = {
-                nix = {
-                  settings = {
-                    auto-optimise-store = true;
-                    sandbox = false;
-                    substituters = [ "https://davegallant.cachix.org" ];
-                    trusted-users = [ "root" "dave" ];
-                    trusted-public-keys = [
-                      "davegallant.cachix.org-1:SsUMqL4+tF2R3/G6X903E9laLlY1rES2QKFfePegF08="
-                    ];
-                  };
-                  registry = { nixpkgs.flake = nixpkgs; };
-                  gc = {
-                    automatic = true;
-                    dates = "daily";
-                    options = "--delete-older-than 14d";
-                  };
-                };
-
-                nixpkgs.overlays = [
-                  (import ./overlays)
-                ];
-
-                home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  users.dave.imports = [
-                    ./home/default.nix
-                    inputs.nixvim.homeManagerModules.nixvim
-                  ];
-                  extraSpecialArgs = {
-                    inherit unstable;
-                  };
-                };
-              };
-            })
-          ];
         in
         {
           hephaestus = nixpkgs.lib.nixosSystem {
             specialArgs = { inherit unstable; };
             modules =
               [
+                ./fonts.nix
                 ./machines/hephaestus/configuration.nix
+                ./packages.nix
                 ./services/netdata/default.nix
-              ]
-              ++ defaultModules;
+                ./upgrade-diff.nix
+                home-manager.nixosModules.home-manager
+
+                ({ config
+                 , lib
+                 , ...
+                 }: {
+                  config = {
+                    nix = {
+                      settings = {
+                        auto-optimise-store = true;
+                        sandbox = false;
+                        substituters = [ "https://davegallant.cachix.org" ];
+                        trusted-users = [ "root" "dave" ];
+                        trusted-public-keys = [
+                          "davegallant.cachix.org-1:SsUMqL4+tF2R3/G6X903E9laLlY1rES2QKFfePegF08="
+                        ];
+                      };
+                      registry = { nixpkgs.flake = nixpkgs; };
+                      gc = {
+                        automatic = true;
+                        dates = "daily";
+                        options = "--delete-older-than 14d";
+                      };
+                    };
+
+                    nixpkgs.overlays = [
+                      (import ./overlays)
+                    ];
+
+                    home-manager = {
+                      useGlobalPkgs = true;
+                      useUserPackages = true;
+                      users.dave.imports = [
+                        ./home/default.nix
+                        inputs.nixvim.homeManagerModules.nixvim
+                      ];
+                      extraSpecialArgs = {
+                        inherit unstable;
+                      };
+                    };
+                  };
+                })
+              ];
           };
         };
 
