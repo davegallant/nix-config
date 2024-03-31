f05e35db78560eb6e5d2592ae0e0113680572e28
Dave Gallant <dave.gallant@gmail.com>
2024-03-31 15:23:44 -0400

    Define pkgs in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,4 +1,9 @@
-(import <nixpkgs> { }).mkShell {
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = { }; overlays = [ ]; };
+in
+
+pkgs.mkShell {
   shellHook = ''
     ${(import ./default.nix).pre-commit-check.shellHook}
   '';
