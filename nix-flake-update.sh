42874502294ade8cd5dc2ed18a1d9079d9adc041
Dave Gallant <dave.gallant@gmail.com>
2025-02-25 22:26:21 -0500

    Add confirmation to git commit and push

--- nix-flake-update.sh
+++ nix-flake-update.sh
@@ -6,6 +6,12 @@ git pull
 update_msg=$(nix flake update 2>&1 | grep -v 'warning:')
 just rebuild
 git add .
+
+read -p "Commit and push changes? [yN]? " -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  exit 1
+fi
+
 git commit -S -m "nix flake update: $(TZ=UTC date '+%Y-%m-%d %H:%M:%S %Z')
 
 $update_msg"
