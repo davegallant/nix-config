5c63022d9639ce0d72093d953d79d7aba34436da
Dave Gallant <davegallant@proton.me>
2026-03-28 20:54:37 -0400

    Add udev rules for sekiro

--- hosts/hephaestus.nix
+++ hosts/hephaestus.nix
@@ -220,6 +220,13 @@
 
   hardware.keyboard.qmk.enable = true;
 
+  services.udev.extraRules = ''
+    # Prevent RDMCTMZT keyboard's extra HID interfaces from being detected as joystick/gamepad
+    ATTRS{idVendor}=="36b0", ATTRS{idProduct}=="3002", \
+    ENV{ID_INPUT_JOYSTICK}="", ENV{ID_INPUT_ACCELEROMETER}="", \
+    ENV{ID_INPUT_KEY}=""
+  '';
+
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
