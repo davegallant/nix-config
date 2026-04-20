048a39a6fbe86f686126ba315f815e1c830bd1e2
Dave Gallant <davegallant@proton.me>
2026-04-19 20:17:59 -0400

    Add kratos.nix

---
+++ hosts/kratos.nix
@@ -0,0 +1,64 @@
+{
+  inputs,
+  pkgs,
+  ...
+}:
+{
+
+  imports = [
+    inputs.niri.nixosModules.niri
+  ];
+
+  nixpkgs.hostPlatform = "aarch64-linux";
+
+  programs.niri = {
+    enable = true;
+    package = pkgs.niri-unstable;
+  };
+
+  networking = {
+    hostName = "kratos";
+  };
+
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    systemd-boot.enable = true;
+  };
+
+  system = {
+    stateVersion = "25.11";
+    activationScripts = {
+      diff = {
+        supportsDryActivation = true;
+        text = ''
+          if [[ -e /run/current-system ]]; then
+            echo -e "\e[36mPackage version diffs:\e[0m"
+            ${pkgs.nvd}/bin/nvd --nix-bin-dir=${pkgs.nix}/bin diff /run/current-system "$systemConfig"
+          fi
+        '';
+      };
+    };
+  };
+
+  nix = {
+    extraOptions = "experimental-features = nix-command flakes";
+    settings.trusted-users = [
+      "root"
+      "@wheel"
+    ];
+  };
+
+  users.users.dave = {
+    isNormalUser = true;
+    extraGroups = [
+      "wheel"
+    ];
+    shell = pkgs.fish;
+  };
+
+  programs.fish.enable = true;
+
+  time.timeZone = "America/Toronto";
+
+  services.sshd.enable = true;
+}
