d16632e3b8a4f7eca66514d91a381aad96c43135
Dave Gallant <davegallant@proton.me>
2026-04-27 21:49:46 -0400

    Pin Niri to 26.04

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     niri = {
-      url = "github:sodiboo/niri-flake";
+      url = "github:sodiboo/niri-flake/e43ef13f23c2c7ae5b10e842745cb345faff4f40"; # 26.04
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nirinit = {
