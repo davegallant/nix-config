e35a29b76c23b98fc7ea84e084aa8414a1a46acc
Dave Gallant <davegallant@proton.me>
2026-05-06 07:20:57 -0400

    Remove line numbers from obsidian

--- home/obsidian.nix
+++ home/obsidian.nix
@@ -25,7 +25,6 @@ in
                 app = {
                   safeMode = false;
                   vimMode = true;
-                  showLineNumber = true;
                 };
                 corePlugins = [
                   "backlink"
