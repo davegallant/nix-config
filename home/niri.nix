ebe0385c9e07842983257d0526a925ae8b283080
Dave Gallant <davegallant@proton.me>
2026-05-24 22:25:33 -0400

    Revert "fix(hephaestus): remove swaylock timer"
    This reverts commit 08a910e61eeb21ca3745753f29601288f15f9919.

--- home/niri.nix
+++ home/niri.nix
@@ -668,6 +668,10 @@
         enable = true;
         timeouts = [
           {
+            timeout = 840; # 14 min: lock before suspend
+            command = "${pkgs.swaylock}/bin/swaylock -f";
+          }
+          {
             timeout = 900; # 15 min: suspend
             command = "${pkgs.systemd}/bin/systemctl suspend";
           }
