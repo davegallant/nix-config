f422cb9c4df50aed6d03ee61e8800b80e9b5be73
Dave Gallant <dave@helios.snake-cloud.ts.net>
2026-06-03 21:17:40 -0400

    Fix ghostty.nix

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -4,7 +4,7 @@
 }:
 {
   xdg.configFile."ghostty/config".text = ''
-    command = /etc/profiles/per-user/dave.gallant/bin/fish
+    command = /run/current-system/sw/bin/fish
     font-size = ${if pkgs.stdenv.isDarwin then "16" else "14"}
     clipboard-trim-trailing-spaces = true
   '';
