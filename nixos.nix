3243eb2a493515e8c2f6fb77be20c0ebf0ad4145
Dave Gallant <dave.gallant@unity3d.com>
2026-06-05 15:50:46 -0400

    refactor(nixos): merge nixos-gui.nix into nixos.nix

--- nixos.nix
+++ nixos.nix
@@ -5,10 +5,6 @@
   ...
 }:
 {
-  imports = [
-    ./nixos-gui.nix
-  ];
-
   system.activationScripts.diff = {
     supportsDryActivation = true;
     text = ''
@@ -81,5 +77,148 @@
   users.users.dave = {
     isNormalUser = true;
     shell = pkgs.fish;
+  };
+
+  environment.systemPackages = with pkgs; [
+    bitwarden-desktop
+    bleachbit
+    borgbackup
+    bruno
+    calibre
+    clamtk
+    evince
+    freefilesync
+    gimp-with-plugins
+    heroic
+    libation
+    libnotify
+    ludusavi
+    lutris
+    mission-center
+    mupen64plus
+    mupdf
+    opensnitch-ui
+    pinta
+    protonup-qt
+    qbittorrent
+    ryubing
+    unstable.ghostty
+    unstable.signal-desktop
+    unstable.spotify
+    unstable.zoom-us
+    via
+    virt-manager
+    vlc
+    wayland-utils
+    wine
+  ];
+
+  fonts.packages = with pkgs; [
+    dejavu_fonts
+    fira-mono
+    font-awesome
+    liberation_ttf
+    nerd-fonts.droid-sans-mono
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.symbols-only
+    nerd-fonts.ubuntu
+    nerd-fonts.ubuntu-mono
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-color-emoji
+  ];
+
+  fonts.fontconfig.defaultFonts = {
+    sansSerif = [ "Noto Sans" ];
+    serif = [ "Noto Serif" ];
+    monospace = [ "Noto Sans Mono" ];
+    emoji = [ "Noto Color Emoji" ];
+  };
+
+  programs.niri = {
+    enable = true;
+    package = pkgs.niri-unstable;
+  };
+
+  services.greetd = {
+    enable = true;
+    settings.default_session = {
+      command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd niri-session";
+      user = "greeter";
+    };
+  };
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5.addons = with pkgs; [
+      fcitx5-anthy
+      fcitx5-gtk
+    ];
+  };
+
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  services.flatpak.enable = true;
+
+  services.nirinit = {
+    enable = true;
+    settings = {
+      launch = {
+        "brave-browser" = "brave";
+        "com.mitchellh.ghostty" = "ghostty";
+        "signal" = "signal-desktop";
+      };
+      skip.apps = [
+        "brave-ophjlpahpchlmihnnnihgmmeilfjmjjc-Default"
+        "opensnitch_ui"
+        "steam"
+      ];
+    };
+  };
+
+  services.tumbler.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+    config.niri.default = [
+      "gtk"
+    ];
+    config.niri."org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+  };
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
+
+  services.printing = {
+    enable = true;
+    browsing = false;
+  };
+  systemd.services.cups-browsed.enable = false;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
   };
 }
