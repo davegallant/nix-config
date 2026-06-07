9805f88d0f0bb266fe25b41bed12a162f8416c0b
Dave Gallant <davegallant@proton.me>
2026-06-06 20:46:19 -0400

    fix(hephaestus): switch back to stable kernel

--- hosts/hephaestus.nix
+++ hosts/hephaestus.nix
@@ -17,7 +17,7 @@
     kernelModules = [
       "kvm-amd"
     ];
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages;
     kernelParams = [
       "amd_iommu=on"
     ];
