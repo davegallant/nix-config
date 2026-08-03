8afafb9580aefd935e11c5773cfde07ad559096a
Dave Gallant <davegallant@gmail.com>
2026-08-03 00:29:04 -0400

    fix(ghostty): reduce font size to 12 on non-Darwin

--- home/ghostty.nix
+++ home/ghostty.nix
@@ -5,7 +5,7 @@
 {
   xdg.configFile."ghostty/config".text = ''
     command = /run/current-system/sw/bin/fish
-    font-size = ${if pkgs.stdenv.isDarwin then "16" else "14"}
+    font-size = ${if pkgs.stdenv.isDarwin then "16" else "12"}
     clipboard-trim-trailing-spaces = true
   '';
 }
