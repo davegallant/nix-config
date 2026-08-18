424bdd78bb9778cedcacebefa9751a89248c7740
Dave Gallant <davegallant@proton.me>
2026-08-17 22:30:37 -0400

    chore(codexbar): pin latest KDE widget revision

--- pkgs/codexbar-kde.nix
+++ pkgs/codexbar-kde.nix
@@ -8,8 +8,8 @@ stdenvNoCC.mkDerivation {
   version = "0.5.0-unstable-2026-08-17";
 
   src = fetchurl {
-    url = "https://codeload.github.com/davegallant/CodexBar-KDE/tar.gz/f883d02bf359012bedd5774c9bbc0129b51ff8dd";
-    hash = "sha256-yLwqdRWb+l6mObSs6c5LQImmLWTVprwb1j6OUrnqWJY=";
+    url = "https://codeload.github.com/davegallant/CodexBar-KDE/tar.gz/a97ead0f68fa8c33d364907a528157a60ae3e5da";
+    hash = "sha256-vPX1JP6eCtzdOJRnlzLAFgExhYF4fkYC7YQoLrR8r7c=";
   };
   dontUnpack = true;
   dontConfigure = true;
